9b99fbc28993b87798ac1f13e5344e02c0be9c3d
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2021-07-26 15:14:43 +0100

    Add prepareRequest & prepareResponse
    This commit adds two additional methods to the SearchService interface,
    prepareRequest & prepareResponse. Stub method implementations are also
    added to the SearchAPI class.

--- src/main/java/org/folio/service/search/SearchAPI.java
+++ src/main/java/org/folio/service/search/SearchAPI.java
@@ -1,21 +1,30 @@
 package org.folio.service.search;
 
 import io.vertx.core.Context;
-import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.model.SearchResponse;
 import org.folio.service.BaseService;
 import org.w3c.dom.Document;
 
+import java.net.http.HttpRequest;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 public class SearchAPI extends BaseService implements SearchService {
 
   @Override
-  @Validate
   public CompletableFuture<SearchResponse> performSearch(Document xcqlDoc, int offset, int limit, Context context, Map<String, String> headers) {
     CompletableFuture<SearchResponse> future = new CompletableFuture<>();
     future.complete(new SearchResponse());
     return future;
+  }
+
+  @Override
+  public HttpRequest prepareRequest(Document search, String baseUrl, int offset, int limit) {
+    return null;
+  }
+
+  @Override
+  public SearchResponse prepareResponse(String response) {
+    return null;
   }
 }
--- src/main/java/org/folio/service/search/SearchService.java
+++ src/main/java/org/folio/service/search/SearchService.java
@@ -4,6 +4,7 @@ import io.vertx.core.Context;
 import org.folio.rest.jaxrs.model.SearchResponse;
 import org.w3c.dom.Document;
 
+import java.net.http.HttpRequest;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
@@ -20,4 +21,26 @@ public interface SearchService {
    * @return collection of search results{@link SearchResponse}
    */
   CompletableFuture<SearchResponse> performSearch(Document search, int offset, int limit, Context context, Map<String, String> headers);
+
+    /**
+    * This method accepts an XCQL org.w3c.dom.Document and returns a search string ready
+    * to be passed to the supplier's search API
+    *
+    * @param search An XCQL document representing the search node tree
+    * @param baseUrl A String representing the supplier's API base URL
+    * @param offset offset
+    * @param limit limit
+    * @return An HttpRequest instance that is ready to be used by the client
+    */
+    HttpRequest prepareRequest(Document search, String baseUrl, int offset, int limit);
+
+    /**
+    * This method receives an HttpResponse containing the supplier's API response
+    * and transforms it into a SearchResponse object
+    *
+    * @param response The supplier's response body
+    * @return A SearchResponse instance
+    */
+    SearchResponse prepareResponse(String response);
+
 }
