c6e3da408e648e6324cae51efbaad9f4a275a018
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2021-09-28 15:52:04 +0100

    Handle empty search strings
    If we haven't received a search string we don't want to proceed with the
    search. This commit throws when such an event occurs.

--- src/main/java/org/folio/rest/impl/ConnectorAPI.java
+++ src/main/java/org/folio/rest/impl/ConnectorAPI.java
@@ -4,6 +4,7 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import org.folio.exception.ConnectorQueryException;
 import org.folio.rest.jaxrs.model.ActionRequest;
 import org.folio.rest.jaxrs.resource.IllConnector;
 import org.folio.service.search.SearchService;
@@ -30,6 +31,9 @@ public class ConnectorAPI extends BaseApi implements IllConnector {
   @Override
   public void getIllConnectorSearch(int offset, int limit, String query, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     // Parse the query into an XCQL document before passing it to performSearch
+    if (query == null || query.length() == 0) {
+      throw new ConnectorQueryException("Query not populated, syntax ?query=title=sleep");
+    }
     CQLUtil parser = new CQLUtil();
     String xcql = parser.parseToXCQL(query);
     XMLUtil xmlParser = new XMLUtil();
