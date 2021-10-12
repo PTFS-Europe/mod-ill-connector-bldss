e34097641a3852d4610de08ab89f6951f4fccb4e
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2021-10-12 12:26:40 +0100

    Return serialised action result
    This commit adds the return of a serialised JSON representation of the
    response to an action request

--- src/main/java/org/folio/util/BLDSSResponse.java
+++ src/main/java/org/folio/util/BLDSSResponse.java
@@ -1,36 +1,40 @@
 package org.folio.util;
 
 import org.folio.exception.ConnectorQueryException;
-import org.json.JSONObject;
 import org.json.XML;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
+
 public class BLDSSResponse {
-  private String xml;
+  private final XMLUtil parser;
+  private final String xml;
   private String timestamp;
   private String status;
   private String message;
   private String result;
 
   public BLDSSResponse(String responseXML) {
+    this.parser = new XMLUtil();
     this.xml = responseXML;
     this.prepareResponse();
   }
 
   public void prepareResponse() {
-
-    XMLUtil parser = new XMLUtil();
-    Document responseDoc = parser.parse(this.xml);
+    Document responseDoc = this.parser.parse(this.xml);
 
     this.timestamp = this.getNode(responseDoc, "timestamp").getTextContent();
     this.status = this.getNode(responseDoc, "status").getTextContent();
     this.message = this.getNode(responseDoc, "message").getTextContent();
-
-    Node result = this.getNode(responseDoc, "result");
-    JSONObject xmlJsonObj = XML.toJSONObject(result.toString());
-    this.result = xmlJsonObj.toString();
+    this.result = this.getJsonResult();
   }
 
   private Node getNode(Document doc, String nodeName) {
@@ -54,6 +58,23 @@ public class BLDSSResponse {
   }
 
   public String getResult() {
-    return result;
+    return this.result;
+  }
+
+  public String getJsonResult() {
+    // We need to take the entire "result" node and convert it to JSON
+    String output = "";
+    StringWriter sw = new StringWriter();
+    Document responseDoc = this.parser.parse(this.xml);
+    Node result = this.getNode(responseDoc, "result");
+    try {
+      Transformer t = TransformerFactory.newInstance().newTransformer();
+      t.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+      t.transform(new DOMSource(result), new StreamResult(sw));
+      output = sw.toString();
+    } catch (TransformerException e) {
+      e.printStackTrace();
+    }
+    return XML.toJSONObject(output).get("result").toString();
   }
 }
